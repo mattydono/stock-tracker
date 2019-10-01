9514f2e989f1ac50d6e8a1f648ba58400b69a183
Egill Hreinson <egillh210@gmail.com>
2019-10-01 14:36:45 +0200

    feat: add buttons with styling

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -11,30 +11,62 @@ import {
     Area,
 } from 'recharts';
 
-const Button = styled.button`
-    width: 20px;
-    height: 20px;
-`
 
 const ChartContainter = styled.div`
     width: 75%;
     height: 40vh;
 `
 
+const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    margin-bottom: 20px;
+    margin-right: 60px;
+`
+
+const Label = styled.label`
+    margin: 0rem 0rem 1rem 0.5rem;
+    display: inline-block;
+    color: inherit;
+    text-decoration: none;
+    font-weight: 100;
+    text-transform: uppercase;
+    cursor: pointer;
+`
+const Input = styled.input`
+    display: none;
+`
+
+
 const RangeButton = ({ range, update, current }) => {
-    const color = current ? 'red' : 'black';
-    return <Button style={{color}} onClick={() => update(range)} >{range}</Button>
+    const opacity = current ? 1.0 : 0.5;
+    return (
+        <Label>
+            <Input 
+                type="radio" 
+                name="chart" 
+                onClick={() => update(range)}
+                defaultChecked={current}
+            />
+            <span style={{opacity}}>{range}</span>
+        </Label>
+    )
 }
+
+
 
 
 const Chart = ({ prices: data, range, updateChartRange }) => {
 
     const ranges = ['1d', '5d', '1m', '1y', '5y'];
 
-    const buttons = ranges.map(rangeItem => <RangeButton current={rangeItem === range} range={rangeItem} update={updateChartRange} />)
+    const buttons = ranges.map(rangeItem => <RangeButton current={rangeItem === range} range={rangeItem} update={updateChartRange} />).reverse();
 
     return (
       <ChartContainter>
+          <ButtonsContainer>
+              {buttons}
+          </ButtonsContainer>
           <ResponsiveContainer aspect={0.9} minWidth={360} maxHeight={500}>
                 <AreaChart data={data} >
                     <CartesianGrid strokeDasharray="3 3" />
