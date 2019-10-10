4cc183ebbee67157cfdf16a0d44cc2b9fbd66a20
Egill Hreinson <egillh210@gmail.com>
2019-10-10 17:50:32 +0200

    fix: throw error in fetchData helper function

--- src/redux/helpers.js
+++ src/redux/helpers.js
@@ -5,9 +5,12 @@ export const createURL = (symbols, endpoint, parameters) => {
 export const fetchData = async (url, callback, error, fetching) => {
     try {
         fetching(true)
-        const result = await fetch(url).then(res => res.json());
-        callback(result);
-        error(false);
+        const response = await fetch(url);
+        if(response.ok) {
+            callback(await response.json());
+            error(false);
+        }
+        else throw new Error(`fetching ${url.split('/')[3]} data unsuccessful`)
     } catch (e) {
         error(e);
     } finally {
