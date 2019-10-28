fa71bbf9862ba845574803c2c184a16079fe870f
Matthew Donovan <matthew.donovan@weareadaptive.com>
2019-10-28 12:21:40 +0000

    index ts

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
