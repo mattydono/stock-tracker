10c30e4c48f27651879b3785af156558299979b5
Matthew Donovan <matthew.donovan@weareadaptive.com>
2019-11-02 21:33:17 +0000

    further footer compentisation

--- src/features/footer/components/footerTickerCard.tsx
+++ src/features/footer/components/footerTickerCard.tsx
@@ -1,62 +1,7 @@
 import React, { memo } from 'react';
 import styled from '@emotion/styled'
 import { _PriceSingleDataPoint } from '../../../models/prices'
-
-const PriceStats = styled.div`
-    font-size: inherit;
-    display: flex;
-    font-weight: normal;
-    @media(max-width: 1000px) {
-        max-height: 60px;
-        justify-content: center;
-    };
-    @media(max-width: 800px) {
-        max-height: 60px;
-        justify-content: center;
-    };
-    @media(max-width: 588px) {
-        font-size: 30px;
-    };
-`
-
-type SpanProps = {
-    color: string
-}
-
-const Span = styled.span<SpanProps>`
-    display: flex;
-    margin-right: 2px;
-    color: ${(props: SpanProps) => props.color};
-`
-
-const PriceIcon = styled.span`
-    font-size: 0.5em;
-    align-self: flex-start;
-    margin-top: 1px;
-`
-
-const PriceSpan = styled.span`
-    display: flex;
-    flex: 0 0 1;
-    margin-right: 10px;
-`
-
-const DollarIcon = styled.div`
-    font-size: 8px;
-    font-weight: 400;
-    margin-top: 1px;
-`
-
-const ChangeContainer = styled.div`
-    display: flex;
-`
-
-type TickerCardPropTypes = {
-    latestPrice: number | undefined,
-    change: number | null,
-    changePercent: number | null,
-    error: boolean,
-}
+import { TickerPrice } from './tickerPrice'
 
 const Ticker = styled.div`
     margin-right: 10px;
@@ -69,7 +14,7 @@ const Ticker = styled.div`
     text-align: left;
 `
 
-const Card = styled.div`
+const PriceCard = styled.div`
     margin-right: 25px;
     display: flex;
     flex: 0 0 1;
@@ -79,57 +24,10 @@ const Card = styled.div`
 const FooterTickerCard: React.FC<_PriceSingleDataPoint> = ({ ticker, latestPrice, change, changePercent, error }) => {
 
     return (
-        <Card>
+        <PriceCard>
             <Ticker>{ticker}</Ticker>
-            <TickerCard error={error} latestPrice={latestPrice} change={change} changePercent={changePercent} />
-        </Card>
-    )
-}
-
-const TickerCard: React.FC<TickerCardPropTypes> = ({ latestPrice, change, changePercent, error }) => {
-
-    return (
-        <PriceStats>
-            {
-                latestPrice ? 
-                    <PriceSpan>
-                        <DollarIcon>$</DollarIcon>
-                        {latestPrice.toFixed(2)}
-                    </PriceSpan> 
-                : null
-            }
-            <ChangeContainer>
-                {
-                    !change ? null 
-                    : change > 0 ? 
-                        <Span color='#91e4a5'>
-                            <PriceIcon>&#129121;</PriceIcon>
-                            {Math.abs(change).toFixed(2)}
-                        </Span> 
-                    : <Span color='#e95656'><PriceIcon>&#129123;</PriceIcon>{Math.abs(change).toFixed(2)}</Span>
-                } 
-                {
-                    !change ? null 
-                    : change > 0 ? 
-                        <Span color='#91e4a5'>|</Span> 
-                    : 
-                        <Span color='#e95656'>|</Span> 
-                }
-                {
-                    !changePercent ? null 
-                    : changePercent > 0 ? 
-                        <Span color='#91e4a5'>
-                            {Math.abs(Math.round((changePercent * 100) * 100) / 100).toFixed(2)}
-                            <PriceIcon>%</PriceIcon>
-                        </Span> 
-                    : 
-                        <Span color='#e95656'>
-                            {Math.abs(Math.round((changePercent * 100) * 100) / 100).toFixed(2)}
-                            <PriceIcon>%</PriceIcon>
-                        </Span>
-                }
-            </ChangeContainer>
-        </PriceStats>
+            <TickerPrice error={error} latestPrice={latestPrice} change={change} changePercent={changePercent} />
+        </PriceCard>
     )
 }
 
