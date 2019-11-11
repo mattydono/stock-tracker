a8b5376add7f785fbae37e17232b3b7bb6e67acf
Matthew Donovan <matthew.donovan@weareadaptive.com>
2019-11-11 11:12:44 +0000

    resetApp 0 payload

--- src/redux/actions/resetApp.ts
+++ src/redux/actions/resetApp.ts
@@ -1,5 +1,5 @@
 import { createAction } from 'redux-actions'
 
 export const RESET_APP_STATE = 'RESET_APP_STATE';
-export const resetState = createAction<undefined>(RESET_APP_STATE);
+export const resetState = createAction<null>(RESET_APP_STATE);
 export type ResetStateAction = ReturnType<typeof resetState>
