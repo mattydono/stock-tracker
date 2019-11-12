a9477aa500c2ff26cf544d0f42c7b4249bcaf37a
Matthew Donovan <matthew.donovan@weareadaptive.com>
2019-11-12 20:53:37 +0000

    icon id changed to unicode character

--- src/utils/getChangeArrowDirection.tsx
+++ src/utils/getChangeArrowDirection.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 
-export const getChangeArrowDirection = (change: number) => change > 0 ? <>&#129121;</> : <>&#129123;</>
+export const getChangeArrowDirection = (change: number) => change > 0 ? '🡡' : '🡣'
     
